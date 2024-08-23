e35973521bd7b5c0c4ee9f2cffe95a7e874b8c8b
Soniya Panda MCA A <146234732+12q-art@users.noreply.github.com>
2024-08-23 13:58:27 +0530

    Create 1.C

---
+++ ASSIGNMENT 1/1.C
@@ -0,0 +1,12 @@
+//PRINT A NAME IN C
+#include <stdio.h>
+
+int main() {
+    // Define the name
+    char name[] = "Alice";
+
+    // Print the name
+    printf("Hello, %s!\n", name);
+
+    return 0;
+}
